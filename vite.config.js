4fcd86a827b223bfb1e0e6f8cf7f0583395cf6c9
Milan Bhingradiya <milanbhingradiya00@gmail.com>
2025-06-04 00:06:00 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // or '/your-subpath/' if deploying under a subdirectory
   plugins: [react()],
 })
